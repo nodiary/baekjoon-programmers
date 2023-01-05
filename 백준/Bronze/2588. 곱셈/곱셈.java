72d99b3a4bbac3281f373a7211cdbcedf4a12a88
nodiary <90733983+nodiary@users.noreply.github.com>
2023-01-05 22:58:19 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,22 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a, b1, b2, b3;
-        String b = "";
-        a = Integer.parseInt(br.readLine());
-        b = br.readLine();
-        b1 = Integer.parseInt(b.substring(0,1));
-        b2 = Integer.parseInt(b.substring(1,2));
-        b3 = Integer.parseInt(b.substring(2,3));
-        System.out.println(a*b3);
-        System.out.println(a*b2);
-        System.out.println(a*b1);
-        System.out.println(a*Integer.parseInt(b));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a*(b%10));
+        System.out.println(a*((b%100)/10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
     }
 }
