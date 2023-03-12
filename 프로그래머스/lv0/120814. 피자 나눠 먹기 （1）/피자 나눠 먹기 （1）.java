dc1bb6ea0d60d99433c438e097214a877daf72c8
nodiary <90733983+nodiary@users.noreply.github.com>
2023-03-12 21:28:27 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
-        int pizza = (n<=7 ?  1 : n/7 + (n%7!=0 ? 1 : 0) );
+        int pizza = n%7 == 0 ? n/7 : n/7 + 1 ;
         return pizza;                       
     }
 }
